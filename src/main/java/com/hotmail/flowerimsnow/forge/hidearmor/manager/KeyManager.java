ef725aa19277992d68ebd54cf5eca6015f3af3e9
Administrator <flowerimsnow@hotmail.com>
2020-11-13 19:49:47 +0800

    修复了没有注册绑定按键的问题 1.0

--- src/main/java/com/hotmail/flowerimsnow/forge/hidearmor/manager/KeyManager.java
+++ src/main/java/com/hotmail/flowerimsnow/forge/hidearmor/manager/KeyManager.java
@@ -4,12 +4,16 @@ import com.hotmail.flowerimsnow.forge.hidearmor.HideArmor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 
 public class KeyManager {
     public static final KeyBinding SWITCH_ARMORS = new KeyBinding("key.switch.armor", Keyboard.KEY_B,"HideArmor");
+    public KeyManager() {
+        ClientRegistry.registerKeyBinding(SWITCH_ARMORS);
+    }
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent e) {
         Minecraft mc = Minecraft.getMinecraft();
